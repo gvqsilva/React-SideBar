01a3c4575a38bf07ccfa00c0a3349adf9c9346a2
gvqsilva <gvqsilva@gmail.com>
2024-11-01 09:06:21 -0300

    fazendo o sideBar

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# React + Vite
+# comando criar branch
+git checkout -b nomedabranch
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+# comando entrar branch
+git checkout branch
 
-Currently, two official plugins are available:
+# comando ver todas as branchs
+git branch
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+# extensão que cria estrutura do react (nutella)
+ES7+ React/Redux/React-Native snipp
+
+# dependencia sidebar
+react-pro-sidebar
--- src/components/SideBar/Side.jsx
+++ src/components/SideBar/Side.jsx
@@ -1,6 +1,33 @@
+import {Sidebar,Menu,MenuItem,SubMenu,sidebarClasses} from 'react-pro-sidebar'
+import {Link} from 'react-router-dom'
 
 export default function Side() {
   return (
-    <div>Side</div>
+    <Sidebar
+    
+      rootStyles={{
+        [`.${sidebarClasses.container}`]:{
+          backgroundColor:"lightgreen", color:"#000"
+        },
+      }}
+
+    
+    >
+      <Menu>
+        <SubMenu label="Dashboard">
+          <MenuItem component={<Link to="/"/>}>Home</MenuItem>
+          <MenuItem component={<Link to="/exemplo1"/>}>Exemplo-1</MenuItem>
+          <MenuItem component={<Link to="exemplo2"/>}>Exemplo-2</MenuItem>
+        </SubMenu>
+        <SubMenu label="Perfil">
+          <MenuItem>Sua Conta</MenuItem>
+          <MenuItem>Avatar</MenuItem>
+          <MenuItem>Sair</MenuItem>
+
+        </SubMenu>
+
+      </Menu>
+
+    </Sidebar>
   )
 }
